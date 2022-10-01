179bb20f19b05e6f3e484af8c9f136f914481993
jason van-rysewyk <jason.marc21@outlook.com>
2022-10-01 13:00:48 +1000

    updated

--- README.md
+++ README.md
@@ -1,24 +1,26 @@
-This is a link to my [live game of wordle](https://jvan-rysewyk.github.io/Wordle_JVR/)
+:upside_down_face: This is a link to my [live game of wordle](https://jvan-rysewyk.github.io/Wordle_JVR/)
 
-
+#**_My first Project_**
 This is my 1st project for the Software Engineering Immersive programe with General Assembly Australia.
 This project was structured to engage not only all the learning that we have had since beggining the program, But also to see if i could push past my limits.
 
-I ran into a few problems along the way and still currently have a problem with the box's changing to green if the letter is in the word twice and is in the correct position.
-I realise the problem is using data-key made things harder for myself to work with, in the i know with this perticular project i could have used (class) and (id) to be able to iterate through the logic easier. 
+:thinking: I ran into a few problems along the way and still currently have a problem with the box's changing to green if the letter is in the word twice and is in the correct position.
+I realise the problem was using data-key making things harder for myself to work with i know with this perticular project i could have used (class) and (id) to be able to iterate through the logic easier, and being able to read duplicate leters in the word. :thinking: 
 
 I am open for any tips if there are any which i am sure there are in assisting me to being able to complete this step.
 
-I also ran into a problem with setting color to the keys once selected not changing color.
+I also ran into a problem with setting color to the keys once selected not changing color and have not been able to affectivly render that problem.
+
+I also ran into a problem with the delete button when i was pressing the delete buttonn on the key board it was deleting the entire contents of the page.
 
 
 
-Technologies, Libraries and Languages Utilized
+##**_Technologies, Libraries and Languages Utilized_**
 
-1. HTML
-2. Css
-3. javaScript
+ - HTML
+ - Css
+ - javaScript
 
 
-
+This project was created off the inspiration of the New York Times news paper to keep minds active and and challange everyday people but also to take a mindful brteak from everyday activities aswell.
 
--- javascript.js
+++ javascript.js
@@ -23,6 +23,7 @@ let alphabets = 0;
 let word = validWords[Math.floor(Math.random() * validWords.length)].toUpperCase();
 console.log(word)
 //selecting all buttons from the HTML file and listening for the click.
+// realised on the day of presentation to fix on eof the problems i have is be to change the way i had created the buttons i should have used a (class) and an (id).
 const buttonElements = document.querySelectorAll('button');
 buttonElements.forEach((element) => {
     element.addEventListener('click', function () {
@@ -30,7 +31,7 @@ buttonElements.forEach((element) => {
 
     });
 });
-// adding the letters from the key board to the page.
+// adding letters to the row 
 function createWord(key) {
     if (letter < 6) {
         wordElements[row - 1].querySelectorAll('.box')[letter - 1].innerText = key;
@@ -39,20 +40,17 @@ function createWord(key) {
 
 }
 
-// checking the word with random word is equal to the letter elements is correct and adding the color to the box.
+// checking the word with random word is equal to the letter elements is correct and adding the color to the box. this is were i wasn't able to have a letter checker to check all letters because of the key-data input above.
 function checkWord() {
  alphabets = 0;
     const letterElements = wordElements[row - 1].querySelectorAll('.box');
-console.log(letterElements)
+// console.log(letterElements)
     letterElements.forEach((element, index) => {
         
         const letter = word.toLowerCase().indexOf(element.innerText.toLowerCase());
 
-
-        if (letterElements === word) {
-            alphabets +=1 ;
-            element.classList.add('box-green');
-        } else if (letter === index) {
+// if the letter is equal to the index of alphabets pend green , yellow , grey to box
+         if (letter === index) {
             alphabets += 1;
             element.classList.add('box-green');
         } else if (letter >= 1) {
@@ -63,7 +61,7 @@ console.log(letterElements)
         }
 
     });
-    // if the word is correctg allert () in you get to your last guess the alert game over()
+    // if the word is correct allert () in you get to your last guess the alert game over()
     if (alphabets === 5) {
         gameOver = true;
         congratulations = true;
@@ -75,6 +73,7 @@ console.log(letterElements)
 
 }
 // if total number of letters is below 6 then alert not enough letters.
+// otherwise continue to the nbext line.
 
 function enterWord() {
     if (letter < 6) {
@@ -86,7 +85,7 @@ function enterWord() {
 
     }
 }
-// 
+// creating a Delete function for when the delete button is pressed on the keyboard. 
 function deleteLetter() {
     const letterElements = wordElements[row - 1].querySelectorAll('.box');
     for (let index = letterElements.length - 1; index >= 0; index--) {
@@ -101,7 +100,7 @@ function deleteLetter() {
     }
 }
 
-
+// keypress is evoking the functions created above and it getting to the end alerting game over.
 
 function keypress(key) {
     if (!gameOver) {
